cf87c5af9a8d2ea5e39b81eef2be61ce81ee6062
Bar-A-94 <balon94@gmail.com>
2024-12-28 00:50:02 +0200

    q5 done

--- A2/code/main.py
+++ A2/code/main.py
@@ -104,7 +104,7 @@ def q3_distance_matrix(mel_dict):
     plt.tight_layout()
     plt.show()
 
-def q5a_collapse_B(string):
+def q4_collapse_B(string):
     collapsed = ""
     for i in range(len(string)):
         if i == 0:
@@ -115,40 +115,44 @@ def q5a_collapse_B(string):
                 collapsed = collapsed + string[i]
     return collapsed
 
-def path_finder(string,labels, matrix):
-    paths = []
-    def genrate_path(string, current_path, index):
-        if index == matrix.shape[0]:
-            if string =="":
-                paths.append(current_path)
-        else:
-            # Move forward in the string
-            if string != "":
-                # If the last character taken is the same as the next one, don't cut the string
-                if current_path!=[] and current_path[-1][1] == labels[string[0]]:
-                    genrate_path(string, current_path + [(index, labels[string[0]])], index + 1)
-                # If it's a new character cut the string, chose the node and move forward
-                else:
-                    genrate_path(string[1:], current_path + [(index, labels[string[0]])], index + 1)
-            # Check the blank character
-            genrate_path(string, current_path +[(index, labels["^"])], index + 1)
-            # Check for last character continuation
-            if current_path!=[]:
-                if current_path[-1][1]!=labels["^"]:
-                    genrate_path(string, current_path +[(index, current_path[-1][1])], index + 1)
+def q5a_initialize_pred():
+    pred = np.zeros(shape=(5, 3), dtype=np.float32)
+    pred[0][0] = 0.8
+    pred[0][1] = 0.2
+    pred[1][0] = 0.2
+    pred[1][1] = 0.8
+    pred[2][0] = 0.3
+    pred[2][1] = 0.7
+    pred[3][0] = 0.09
+    pred[3][1] = 0.8
+    pred[3][2] = 0.11
+    pred[4][2] = 1.00
+    labels = {'a': 0,'b': 1, '^':2}
 
-    genrate_path(string, [], 0)
-    return paths
+    return pred, labels
 
 def q5_ctc_forward(string, labels, matrix):
-    paths = path_finder(string, labels, matrix)
-    total_prob = 0
-    for path in paths:
-        path_prob = 1
-        for node in path:
-            path_prob *= matrix[node]
-        total_prob += path_prob
-    return total_prob
+    # Extend ground truth with blanks
+    blank = "^"
+    extended_string="^"
+    for c in string:
+        extended_string += c + blank
+    forward_matrix = np.zeros((matrix.shape[0], len(extended_string)))
+    forward_matrix[0, 0] = matrix[0, labels["^"]]
+    forward_matrix[0, 1] = matrix[0, labels[extended_string[1]]]
+    for t in range(1, matrix.shape[0]):
+        for l in range(0,len(extended_string)):
+            prob_l = matrix[t, labels[extended_string[l]]]
+            # Stay at current level
+            forward_matrix[t, l] = prob_l * forward_matrix[t - 1, l]
+            # Transition from previous label
+            if l > 0:
+                forward_matrix[t, l] += prob_l * forward_matrix[t - 1, l - 1]
+            # Transition with skiping over blank
+            if l > 1 and extended_string[l] != extended_string[l - 2]:
+                forward_matrix[t, l] += prob_l * forward_matrix[t - 1, l - 2]
+    plot_pred(forward_matrix, labels)
+    return forward_matrix[-1, -1] + forward_matrix[-1, -2]
 
 def plot_pred(pred, labels):
     """
@@ -197,29 +201,18 @@ if __name__ == "__main__":
     # q3_distance_matrix(mel_dict)
 
     # Question 5 - CTC
-    # Define pred by 5.b
-    pred = np.zeros(shape=(5, 3), dtype=np.float32)
-    pred[0][0] = 0.8
-    pred[0][1] = 0.2
-    pred[1][0] = 0.2
-    pred[1][1] = 0.8
-    pred[2][0] = 0.3
-    pred[2][1] = 0.7
-    pred[3][0] = 0.09
-    pred[3][1] = 0.8
-    pred[3][2] = 0.11
-    pred[4][2] = 1.00
-    log_pred = np.log(pred)
-
-    labels = {'a': 0,'b': 1, '^':2}
-
+    pred, labels = q5a_initialize_pred()
     # plot_pred(pred, labels)
 
+    # log_pred = np.log(pred)
     # plot_pred(log_pred, labels)
 
-    print(q5_ctc_forward("abb",labels, pred)) # 0.85
-    print(q5_ctc_forward("ab",labels, pred)) # 0.54
-    print(q5_ctc_forward("aba",labels, pred)) # 0.88
+    # print(q5_ctc_forward("abb",labels, pred)) # 0.00
+    # print(q5_ctc_forward("ab",labels, pred)) # 0.548
+    print(q5_ctc_forward("aba",labels, pred)) # 0.088
+
+
+
 
 
 
